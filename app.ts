80897d1cf482c1f9a2f369cb3d6ea8bf494937b6
Assaf Basri <Faskoman90@gmail.com>
2023-11-20 21:05:44 +0200

    fixed form submition

--- app.ts
+++ app.ts
@@ -12,43 +12,46 @@ type ActivityArray = Activity[];
 
 const activities: ActivityArray = [];
 
-function addActivity(e: Event) {
-//   e.preventDefault();
-  console.log("Form submitted!");
+const logActivityForm = document.querySelector("form[name='log-activity']");
+if (!logActivityForm) {
+  console.error("Couldn't find log activity form.");
+} else {
+  logActivityForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    console.log("Form submitted!");
 
-  const nameInput = document.getElementById(
-    "activity-name"
-  ) as HTMLInputElement;
-  const typeSelect = document.getElementById(
-    "activity-type"
-  ) as HTMLSelectElement;
-  const durationInput = document.getElementById(
-    "activity-duration"
-  ) as HTMLInputElement;
-  const dateInput = document.getElementById(
-    "activity-date"
-  ) as HTMLInputElement;
-  const locationInput = document.getElementById(
-    "activity-location"
-  ) as HTMLInputElement;
+    const nameInput = document.getElementById(
+      "activity-name"
+    ) as HTMLInputElement;
+    const typeSelect = document.getElementById(
+      "activity-type"
+    ) as HTMLSelectElement;
+    const durationInput = document.getElementById(
+      "activity-duration"
+    ) as HTMLInputElement;
+    const dateInput = document.getElementById(
+      "activity-date"
+    ) as HTMLInputElement;
+    const locationInput = document.getElementById(
+      "activity-location"
+    ) as HTMLInputElement;
 
-  const name = nameInput.value;
-  const type = typeSelect.value as ActivityType;
-  const duration = Number(durationInput.value);
-  const date = dateInput.value;
-  const location = locationInput.value;
+    const name = nameInput.value;
+    const type = typeSelect.value as ActivityType;
+    const duration = Number(durationInput.value);
+    const date = dateInput.value;
+    const location = locationInput.value;
 
-  const newActivity: Activity = {
-    activityName: name,
-    activityType: type,
-    activityDuration: duration,
-    activityDate: date,
-    activityLocation: location,
-  };
+    const newActivity: Activity = {
+      activityName: name,
+      activityType: type,
+      activityDuration: duration,
+      activityDate: date,
+      activityLocation: location,
+    };
 
-  console.log("New activity added:", newActivity);
-  activities.push(newActivity);
-  console.log("All activities:", activities);
-
-  console.log(activities);
+    console.log("New activity added:", newActivity);
+    activities.push(newActivity);
+    console.log("All activities:", activities);
+  });
 }
--- dist/app.js
+++ dist/app.js
@@ -1,26 +1,31 @@
 const activities = [];
-function addActivity(e) {
-    //   e.preventDefault();
-    console.log("Form submitted!");
-    const nameInput = document.getElementById("activity-name");
-    const typeSelect = document.getElementById("activity-type");
-    const durationInput = document.getElementById("activity-duration");
-    const dateInput = document.getElementById("activity-date");
-    const locationInput = document.getElementById("activity-location");
-    const name = nameInput.value;
-    const type = typeSelect.value;
-    const duration = Number(durationInput.value);
-    const date = dateInput.value;
-    const location = locationInput.value;
-    const newActivity = {
-        activityName: name,
-        activityType: type,
-        activityDuration: duration,
-        activityDate: date,
-        activityLocation: location,
-    };
-    console.log("New activity added:", newActivity);
-    activities.push(newActivity);
-    console.log("All activities:", activities);
-    console.log(activities);
+const logActivityForm = document.querySelector("form[name='log-activity']");
+if (!logActivityForm) {
+    console.error("Couldn't find log activity form.");
+}
+else {
+    logActivityForm.addEventListener("submit", function (e) {
+        e.preventDefault();
+        console.log("Form submitted!");
+        const nameInput = document.getElementById("activity-name");
+        const typeSelect = document.getElementById("activity-type");
+        const durationInput = document.getElementById("activity-duration");
+        const dateInput = document.getElementById("activity-date");
+        const locationInput = document.getElementById("activity-location");
+        const name = nameInput.value;
+        const type = typeSelect.value;
+        const duration = Number(durationInput.value);
+        const date = dateInput.value;
+        const location = locationInput.value;
+        const newActivity = {
+            activityName: name,
+            activityType: type,
+            activityDuration: duration,
+            activityDate: date,
+            activityLocation: location,
+        };
+        console.log("New activity added:", newActivity);
+        activities.push(newActivity);
+        console.log("All activities:", activities);
+    });
 }
