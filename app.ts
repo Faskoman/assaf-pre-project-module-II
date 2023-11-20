45076d473509e5bddcc501785080b14fcf00bc1a
Assaf Basri <Faskoman90@gmail.com>
2023-11-20 20:04:11 +0200

    newActivity object to push into activities[]

--- app.ts
+++ app.ts
@@ -12,7 +12,9 @@ type ActivityArray = Activity[];
 
 const activities: ActivityArray = [];
 
-function addActivity() {
+function addActivity(e: Event) {
+  e.preventDefault();
+
   const nameInput = document.getElementById(
     "activity-name"
   ) as HTMLInputElement;
@@ -29,6 +31,21 @@ function addActivity() {
     "activity-location"
   ) as HTMLInputElement;
 
+  const name = nameInput.value;
+  const type = typeSelect.value as ActivityType;
+  const duration = Number(durationInput.value);
+  const date = dateInput.value;
+  const location = locationInput.value;
+
+  const newActivity: Activity = {
+    activityName: name,
+    activityType: type,
+    activityDuration: duration,
+    activityDate: date,
+    activityLocation: location,
+  };
+
+  activities.push(newActivity);
+
   console.log(activities);
 }
-
--- dist/app.js
+++ dist/app.js
@@ -1,9 +1,23 @@
 const activities = [];
-function addActivity() {
+function addActivity(e) {
+    e.preventDefault();
     const nameInput = document.getElementById("activity-name");
     const typeSelect = document.getElementById("activity-type");
     const durationInput = document.getElementById("activity-duration");
     const dateInput = document.getElementById("activity-date");
     const locationInput = document.getElementById("activity-location");
+    const name = nameInput.value;
+    const type = typeSelect.value;
+    const duration = Number(durationInput.value);
+    const date = dateInput.value;
+    const location = locationInput.value;
+    const newActivity = {
+        activityName: name,
+        activityType: type,
+        activityDuration: duration,
+        activityDate: date,
+        activityLocation: location,
+    };
+    activities.push(newActivity);
     console.log(activities);
 }
