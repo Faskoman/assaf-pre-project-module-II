5e244484492760bf61b9aaad62a2a970e9687218
Assaf Basri <Faskoman90@gmail.com>
2023-11-21 16:04:56 +0200

    if selected remove display none

--- dist/track.js
+++ dist/track.js
@@ -1,20 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
     const trackActivityForm = document.querySelector("form[name='track-activity']");
     const activityTypeSelect = document.getElementById("track-activity-type");
+    const activityNameDiv = document.getElementById("activity-name-div");
     const activityNameInput = document.getElementById("activity-name");
+    const activityLocationDiv = document.getElementById("activity-location-div");
     const activityLocationInput = document.getElementById("activity-location");
-    const startButton = document.querySelector("button.--display-none");
-    const pauseButton = document.querySelectorAll("button.--display-none")[1];
-    const finishButton = document.querySelectorAll("button.--display-none")[2];
+    const startButton = document.getElementById("start-button");
+    const pauseButton = document.getElementById("pause-button");
+    const finishButton = document.getElementById("finish-button");
     activityTypeSelect.addEventListener("change", function () {
         const selectedType = activityTypeSelect.value;
         if (selectedType) {
-            activityNameInput.classList.remove("--display-none");
-            activityLocationInput.classList.remove("--display-none");
+            activityNameDiv.classList.remove("--display-none");
+            activityLocationDiv.classList.remove("--display-none");
         }
         else {
-            activityNameInput.classList.add("--display-none");
-            activityLocationInput.classList.add("--display-none");
+            activityNameDiv.classList.add("--display-none");
+            activityLocationDiv.classList.add("--display-none");
         }
     });
     activityNameInput.addEventListener("input", function () {
@@ -29,6 +31,7 @@ document.addEventListener("DOMContentLoaded", function () {
     startButton.addEventListener("click", function () {
         pauseButton.classList.remove("--display-none");
         finishButton.classList.remove("--display-none");
+        startButton.classList.add("--display-none");
     });
     trackActivityForm?.addEventListener("submit", function (e) {
         e.preventDefault();
--- track.ts
+++ track.ts
@@ -3,21 +3,23 @@ import { ActivityType , Activity , ActivityArray , activities } from "./activiti
 document.addEventListener("DOMContentLoaded", function () {
     const trackActivityForm = document.querySelector("form[name='track-activity']") as HTMLFormElement;
     const activityTypeSelect = document.getElementById("track-activity-type") as HTMLSelectElement;
+    const activityNameDiv = document.getElementById("activity-name-div") as HTMLElement;
     const activityNameInput = document.getElementById("activity-name") as HTMLInputElement;
+    const activityLocationDiv = document.getElementById("activity-location-div") as HTMLElement;
     const activityLocationInput = document.getElementById("activity-location") as HTMLInputElement;
-    const startButton = document.querySelector("button.--display-none") as HTMLButtonElement;
-    const pauseButton = document.querySelectorAll("button.--display-none")[1] as HTMLButtonElement;
-    const finishButton = document.querySelectorAll("button.--display-none")[2] as HTMLButtonElement;
+    const startButton = document.getElementById("start-button") as HTMLButtonElement;
+    const pauseButton = document.getElementById("pause-button") as HTMLButtonElement;
+    const finishButton = document.getElementById("finish-button") as HTMLButtonElement;
   
     activityTypeSelect.addEventListener("change", function () {
       const selectedType = activityTypeSelect.value;
   
       if (selectedType) {
-        activityNameInput.classList.remove("--display-none");
-        activityLocationInput.classList.remove("--display-none");
+        activityNameDiv.classList.remove("--display-none");
+        activityLocationDiv.classList.remove("--display-none");
       } else {
-        activityNameInput.classList.add("--display-none");
-        activityLocationInput.classList.add("--display-none");
+        activityNameDiv.classList.add("--display-none");
+        activityLocationDiv.classList.add("--display-none");
       }
     });
   
@@ -34,6 +36,7 @@ document.addEventListener("DOMContentLoaded", function () {
     startButton.addEventListener("click", function () {
       pauseButton.classList.remove("--display-none");
       finishButton.classList.remove("--display-none");
+      startButton.classList.add("--display-none");
     });
   
     trackActivityForm?.addEventListener("submit", function (e) {
